b4150c6c466e5c004f948e7355e1845e1a958a00
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2021-03-04 22:00:13 +0000

    add my project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace detect_secrets_action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            secret="My super secret key"
+            Console.WriteLine(String.Format("My secret value is {0}", secret));
+        }
+    }
+}
